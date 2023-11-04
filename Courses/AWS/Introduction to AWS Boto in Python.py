93da6df848da2a2e22472241932c88c55674cbce
sabirinID <108510388+sabirinID@users.noreply.github.com>
2023-11-05 06:12:29 +0700

    Upload file

---
+++ Courses/AWS/Introduction to AWS Boto in Python.py
@@ -0,0 +1,94 @@
+## Your first boto3 client
+
+# Generate the boto3 client for interacting with S3
+s3 = boto3.client('s3', region_name='us-east-1', 
+                        # Set up AWS credentials 
+                  		aws_access_key_id=AWS_KEY_ID, 
+                        aws_secret_access_key=AWS_SECRET)
+# List the buckets
+buckets = s3.list_buckets()
+
+# Print the buckets
+print(buckets)
+
+## Multiple clients
+
+# Generate the boto3 client for interacting with S3 and SNS
+s3 = boto3.client('s3', region_name='us-east-1', 
+                         aws_access_key_id=AWS_KEY_ID, 
+                         aws_secret_access_key=AWS_SECRET)
+
+sns = boto3.client('sns', region_name='us-east-1', 
+                           aws_access_key_id=AWS_KEY_ID, 
+                           aws_secret_access_key=AWS_SECRET)
+
+# List S3 buckets and SNS topics
+buckets = s3.list_buckets()
+topics = sns.list_topics()
+
+# Print out the list of SNS topics
+print(topics)
+
+## Removing repetitive work
+
+press 5 # Stupendous! You are correct! To build Sam's idea, all of these AWS services would have to work together!
+
+## Creating a bucket
+
+import boto3
+
+# Create boto3 client to S3
+s3 = boto3.client('s3', region_name='us-east-1', 
+                         aws_access_key_id=AWS_KEY_ID, 
+                         aws_secret_access_key=AWS_SECRET)
+
+# Create the buckets
+response_staging = s3.create_bucket(Bucket='gim-staging')
+response_processed = s3.create_bucket(Bucket='gim-processed')
+response_test = s3.create_bucket(Bucket='gim-test')
+
+# Print out the response
+print(response_staging)
+
+## Listing buckets
+
+# Get the list_buckets response
+response = s3.list_buckets()
+
+# Iterate over Buckets from .list_buckets() response
+for bucket in response['Buckets']:
+  
+  	# Print the Name for each bucket
+    print(bucket['Name'])
+
+## Deleting a bucket
+
+# Delete the gim-test bucket
+s3.delete_bucket(Bucket='gim-test')
+
+# Get the list_buckets response
+response = s3.list_buckets()
+
+# Print each Buckets Name
+for bucket in response['Buckets']:
+    print(bucket['Name'])
+
+## Deleting multiple buckets
+
+# Get the list_buckets response
+response = s3.list_buckets()
+
+# Delete all the buckets with 'gim', create replacements.
+for bucket in response['Buckets']:
+  if 'gim' in bucket['Name']:
+      s3.delete_bucket(Bucket=bucket['Name'])
+    
+s3.create_bucket(Bucket='gid-staging')
+s3.create_bucket(Bucket='gid-processed')
+  
+# Print bucket listing after deletion
+response = s3.list_buckets()
+for bucket in response['Buckets']:
+    print(bucket['Name'])
+
+## 
