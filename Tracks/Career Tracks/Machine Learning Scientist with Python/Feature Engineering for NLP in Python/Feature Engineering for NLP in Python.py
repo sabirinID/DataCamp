6297bd074e4bb65931604f12013d1e38c8e7a197
sabirinID <108510388+sabirinID@users.noreply.github.com>
2023-11-06 00:04:39 +0700

    Upload file

---
+++ Tracks/Career Tracks/Machine Learning Scientist with Python/Feature Engineering for NLP in Python/Feature Engineering for NLP in Python.py
@@ -0,0 +1,93 @@
+## Data format for ML algorithms
+
+"""
+In [1]:
+df3.dtypes
+Out[1]:
+
+feature 1    int64
+feature 2    int64
+feature 3    int64
+feature 4    int64
+feature 5    int64
+label        int64
+dtype: object
+"""
+
+df3 # Excellent! This dataframe has numerical training features and the predictor variable is a class. Therefore, it is in a suitable format for applying a classification algorithm.
+
+## One-hot encoding
+
+# Print the features of df1
+print(df1.columns)
+
+# Perform one-hot encoding
+df1 = pd.get_dummies(df1, columns=['feature 5'])
+
+# Print the new features of df1
+print(df1.columns)
+
+# Print first five rows of df1
+print(df1.head())
+
+## Character count of Russian tweets
+
+# Create a feature char_count
+tweets['char_count'] = tweets['content'].apply(len)
+
+# Print the average character count
+print(tweets['char_count'].mean())
+
+## Word count of TED talks
+
+# Function that returns number of words in a string
+def count_words(string):
+	# Split the string into words
+    words = string.split()
+    
+    # Return the number of words
+    return len(words)
+
+# Create a new feature word_count
+ted['word_count'] = ted['transcript'].apply(count_words)
+
+# Print the average word count of the talks
+print(ted['word_count'].mean())
+
+## Hashtags and mentions in Russian tweets
+
+# Function that returns number of hashtags in a string
+def count_hashtags(string):
+	# Split the string into words
+    words = string.split()
+    
+    # Create a list of words that are hashtags
+    hashtags = [word for word in words if word.startswith('#')]
+    
+    # Return number of hashtags
+    return(len(hashtags))
+
+# Create a feature hashtag_count and display distribution
+tweets['hashtag_count'] = tweets['content'].apply(count_hashtags)
+tweets['hashtag_count'].hist()
+plt.title('Hashtag count distribution')
+plt.show()
+
+# Function that returns number of mentions in a string
+def count_mentions(string):
+	# Split the string into words
+    words = string.split()
+    
+    # Create a list of words that are mentions
+    mentions = [word for word in words if word.startswith('@')]
+    
+    # Return number of mentions
+    return(len(mentions))
+
+# Create a feature mention_count and display distribution
+tweets['mention_count'] = tweets['content'].apply(count_mentions)
+tweets['mention_count'].hist()
+plt.title('Mention count distribution')
+plt.show()
+
+## 
