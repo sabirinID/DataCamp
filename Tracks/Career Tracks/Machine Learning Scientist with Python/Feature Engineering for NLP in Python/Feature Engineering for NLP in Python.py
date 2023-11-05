fc21ba03013ae617e4414f7bcd755c5eb9a5c8ba
sabirinID <108510388+sabirinID@users.noreply.github.com>
2023-11-06 00:20:24 +0700

    Update file

--- Tracks/Career Tracks/Machine Learning Scientist with Python/Feature Engineering for NLP in Python/Feature Engineering for NLP in Python.py
+++ Tracks/Career Tracks/Machine Learning Scientist with Python/Feature Engineering for NLP in Python/Feature Engineering for NLP in Python.py
@@ -90,4 +90,495 @@ tweets['mention_count'].hist()
 plt.title('Mention count distribution')
 plt.show()
 
-## 
+## Readability of 'The Myth of Sisyphus'
+
+# Import Textatistic
+from textatistic import Textatistic
+
+# Compute the readability scores 
+readability_scores = Textatistic(sisyphus_essay).scores
+
+# Print the flesch reading ease score
+flesch = readability_scores['flesch_score']
+print("The Flesch Reading Ease is %.2f" % (flesch))
+
+## Readability of various publications
+
+# Import Textatistic
+from textatistic import Textatistic
+
+# List of excerpts
+excerpts = [forbes, harvard_law, r_digest, time_kids]
+
+# Loop through excerpts and compute gunning fog index
+gunning_fog_scores = []
+for excerpt in excerpts:
+  readability_scores = Textatistic(excerpt).scores
+  gunning_fog = readability_scores['gunningfog_score']
+  gunning_fog_scores.append(gunning_fog)
+  
+# Print the gunning fog indices
+print(gunning_fog_scores)
+
+## Identifying lemmas
+
+press 4 # Car, Bike, Truck, Bus
+
+# Good job! Although all these words refer to vehicles, they are words with distinct base forms.
+
+## Tokenizing the Gettysburg Address
+
+import spacy
+
+# Load the en_core_web_sm model
+nlp = spacy.load('en_core_web_sm')
+
+# Create a Doc object
+doc = nlp(gettysburg)
+
+# Generate the tokens
+tokens = [token.text for token in doc]
+print(tokens)
+
+## Lemmatizing the Gettysburg address
+
+# Print the gettysburg address
+print(gettysburg)
+
+import spacy
+
+# Load the en_core_web_sm model
+nlp = spacy.load('en_core_web_sm')
+
+# Create a Doc object
+doc = nlp(gettysburg)
+
+# Generate lemmas
+lemmas = [token.lemma_ for token in doc]
+
+# Convert lemmas into a string
+print(' '.join(lemmas))
+
+## Cleaning a blog post
+
+# Load model and create Doc object
+nlp = spacy.load('en_core_web_sm')
+doc = nlp(blog)
+
+# Generate lemmatized tokens
+lemmas = [token.lemma_ for token in doc]
+
+# Remove stopwords and non-alphabetic tokens
+a_lemmas = [lemma for lemma in lemmas 
+            if lemma.isalpha() and lemma not in stopwords]
+
+# Print string after text cleaning
+print(' '.join(a_lemmas))
+
+## Cleaning TED talks in a dataframe
+
+# Function to preprocess text
+def preprocess(text):
+  	# Create Doc object
+    doc = nlp(text, disable=['ner', 'parser'])
+    # Generate lemmas
+    lemmas = [token.lemma_ for token in doc]
+    # Remove stopwords and non-alphabetic characters
+    a_lemmas = [lemma for lemma in lemmas 
+            if lemma.isalpha() and lemma not in stopwords]
+    
+    return ' '.join(a_lemmas)
+  
+# Apply preprocess to ted['transcript']
+ted['transcript'] = ted['transcript'].apply(preprocess)
+print(ted['transcript'])
+
+## POS tagging in Lord of the Flies
+
+# Load the en_core_web_sm model
+nlp = spacy.load('en_core_web_sm')
+
+# Create a Doc object
+doc = nlp(lotf)
+
+# Generate tokens and pos tags
+pos = [(token.text, token.pos_) for token in doc]
+print(pos)
+
+## Counting nouns in a piece of text
+
+nlp = spacy.load('en_core_web_sm')
+
+# Returns number of proper nouns
+def proper_nouns(text, model=nlp):
+  	# Create doc object
+    doc = model(text)
+    # Generate list of POS tags
+    pos = [token.pos_ for token in doc]
+    
+    # Return number of proper nouns
+    return pos.count('PROPN')
+
+print(proper_nouns("Abdul, Bill and Cathy went to the market to buy apples.", nlp))
+
+nlp = spacy.load('en_core_web_sm')
+
+# Returns number of other nouns
+def nouns(text, model=nlp):
+  	# Create doc object
+    doc = model(text)
+    # Generate list of POS tags
+    pos = [token.pos_ for token in doc]
+    
+    # Return number of other nouns
+    return pos.count('NOUN')
+
+print(nouns("Abdul, Bill and Cathy went to the market to buy apples.", nlp))
+
+## Noun usage in fake news
+
+headlines['num_propn'] = headlines['title'].apply(proper_nouns)
+
+# Compute mean of proper nouns
+real_propn = headlines[headlines['label'] == 'REAL']['num_propn'].mean()
+fake_propn = headlines[headlines['label'] == 'FAKE']['num_propn'].mean()
+
+# Print results
+print("Mean no. of proper nouns in real and fake headlines are %.2f and %.2f respectively"%(real_propn, fake_propn))
+
+headlines['num_noun'] = headlines['title'].apply(nouns)
+
+# Compute mean of other nouns
+real_noun = headlines[headlines['label'] == 'REAL']['num_noun'].mean()
+fake_noun = headlines[headlines['label'] == 'FAKE']['num_noun'].mean()
+
+# Print results
+print("Mean no. of other nouns in real and fake headlines are %.2f and %.2f respectively"%(real_noun, fake_noun))
+
+## Named entities in a sentence
+
+# Load the required model
+nlp = spacy.load('en_core_web_sm')
+
+# Create a Doc instance 
+text = 'Sundar Pichai is the CEO of Google. Its headquarters is in Mountain View.'
+doc = nlp(text)
+
+# Print all named entities and their labels
+for ent in doc.ents:
+    print(ent.text, ent.label_)
+
+## Identifying people mentioned in a news article
+
+def find_persons(text):
+  # Create Doc object
+  doc = nlp(text)
+  
+  # Identify the persons
+  persons = [ent.text for ent in doc.ents if ent.label_ == 'PERSON']
+  
+  # Return persons
+  return persons
+
+print(find_persons(tc))
+
+## Word vectors with a given vocabulary
+
+press 1 # (0, 0, 1, 1, 0, 1, 0, 1, 2, 1, 2, 2, 1, 0, 0)
+
+# Good job! That is, indeed, the correct answer. Each value in the vector corresponds to the frequency of the corresponding word in the vocabulary.
+
+## BoW model for movie taglines
+
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Create CountVectorizer object
+vectorizer = CountVectorizer()
+
+# Generate matrix of word vectors
+bow_matrix = vectorizer.fit_transform(corpus)
+
+# Print the shape of bow_matrix
+print(bow_matrix.shape)
+
+## Analyzing dimensionality and preprocessing
+
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Create CountVectorizer object
+vectorizer = CountVectorizer()
+
+# Generate matrix of word vectors
+bow_lem_matrix = vectorizer.fit_transform(lem_corpus)
+
+# Print the shape of bow_lem_matrix
+print(bow_lem_matrix.shape)
+
+## Mapping feature indices with feature names
+
+# Create CountVectorizer object
+vectorizer = CountVectorizer()
+
+# Generate matrix of word vectors
+bow_matrix = vectorizer.fit_transform(corpus)
+
+# Convert bow_matrix into a DataFrame
+bow_df = pd.DataFrame(bow_matrix.toarray())
+
+# Map the column names to vocabulary 
+bow_df.columns = vectorizer.get_feature_names()
+
+# Print bow_df
+print(bow_df)
+
+## BoW vectors for movie reviews
+
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Create a CountVectorizer object
+vectorizer = CountVectorizer(lowercase=True, stop_words='english')
+
+# Fit and transform X_train
+X_train_bow = vectorizer.fit_transform(X_train)
+
+# Transform X_test
+X_test_bow = vectorizer.transform(X_test)
+
+# Print shape of X_train_bow and X_test_bow
+print(X_train_bow.shape)
+print(X_test_bow.shape)
+
+## Predicting the sentiment of a movie review
+
+# Create a MultinomialNB object
+clf = MultinomialNB()
+
+# Fit the classifier
+clf.fit(X_train_bow, y_train)
+
+# Measure the accuracy
+accuracy = clf.score(X_test_bow, y_test)
+print("The accuracy of the classifier on the test set is %.3f" % accuracy)
+
+# Predict the sentiment of a negative review
+review = "The movie was terrible. The music was underwhelming and the acting mediocre."
+prediction = clf.predict(vectorizer.transform([review]))[0]
+print("The sentiment predicted by the classifier is %i" % (prediction))
+
+## n-gram models for movie tag lines
+
+# Generate n-grams upto n=1
+vectorizer_ng1 = CountVectorizer(ngram_range=(1,1))
+ng1 = vectorizer_ng1.fit_transform(corpus)
+
+# Generate n-grams upto n=2
+vectorizer_ng2 = CountVectorizer(ngram_range=(1,2))
+ng2 = vectorizer_ng2.fit_transform(corpus)
+
+# Generate n-grams upto n=3
+vectorizer_ng3 = CountVectorizer(ngram_range=(1,3))
+ng3 = vectorizer_ng3.fit_transform(corpus)
+
+# Print the number of features for each model
+print("ng1, ng2 and ng3 have %i, %i and %i features respectively" % (ng1.shape[1], ng2.shape[1], ng3.shape[1]))
+
+## Higher order n-grams for sentiment analysis
+
+# Define an instance of MultinomialNB 
+clf_ng = MultinomialNB()
+
+# Fit the classifier
+clf_ng.fit(X_train_ng, y_train)
+
+# Measure the accuracy
+accuracy = clf_ng.score(X_test_ng, y_test)
+print("The accuracy of the classifier on the test set is %.3f" % accuracy)
+
+# Predict the sentiment of a negative review
+review = "The movie was not good. The plot had several holes and the acting lacked panache."
+prediction = clf_ng.predict(ng_vectorizer.transform([review]))[0]
+print("The sentiment predicted by the classifier is %i" % (prediction))
+
+## Comparing performance of n-gram models
+
+start_time = time.time()
+# Splitting the data into training and test sets
+train_X, test_X, train_y, test_y = train_test_split(df['review'], df['sentiment'], test_size=0.5, random_state=42, stratify=df['sentiment'])
+
+# Generating ngrams
+vectorizer = CountVectorizer()
+train_X = vectorizer.fit_transform(train_X)
+test_X = vectorizer.transform(test_X)
+
+# Fit classifier
+clf = MultinomialNB()
+clf.fit(train_X, train_y)
+
+# Print accuracy, time and number of dimensions
+print("The program took %.3f seconds to complete. The accuracy on the test set is %.2f. The ngram representation had %i features." % (time.time() - start_time, clf.score(test_X, test_y), train_X.shape[1]))
+
+start_time = time.time()
+# Splitting the data into training and test sets
+train_X, test_X, train_y, test_y = train_test_split(df['review'], df['sentiment'], test_size=0.5, random_state=42, stratify=df['sentiment'])
+
+# Generating ngrams
+vectorizer = CountVectorizer(ngram_range=(1,3))
+train_X = vectorizer.fit_transform(train_X)
+test_X = vectorizer.transform(test_X)
+
+# Fit classifier
+clf = MultinomialNB()
+clf.fit(train_X, train_y)
+
+# Print accuracy, time and number of dimensions
+print("The program took %.3f seconds to complete. The accuracy on the test set is %.2f. The ngram representation had %i features." % (time.time() - start_time, clf.score(test_X, test_y), train_X.shape[1]))
+
+## tf-idf weight of commonly occurring words
+
+press 1 # 0
+
+# Correct! In fact, the tf-idf weight for bottle in every document will be 0. This is because the inverse document frequency is constant across documents in a corpus and since bottle occurs in every document, its value is log(1), which is 0.
+
+## tf-idf vectors for TED talks
+
+# Import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Create TfidfVectorizer object
+vectorizer = TfidfVectorizer()
+
+# Generate matrix of word vectors
+tfidf_matrix = vectorizer.fit_transform(ted)
+
+# Print the shape of tfidf_matrix
+print(tfidf_matrix.shape)
+
+## Range of cosine scores
+
+press 1 # 0.86
+
+# Great job! Since document vectors use only non-negative weights, the cosine score lies between 0 and 1.
+
+## Computing dot product
+
+# Initialize numpy vectors
+A = np.array([1,3])
+B = np.array([-2, 2])
+
+# Compute dot product
+dot_prod = np.dot(A, B)
+
+# Print dot product
+print(dot_prod)
+
+## Cosine similarity matrix of a corpus
+
+# Initialize an instance of tf-idf Vectorizer
+tfidf_vectorizer = TfidfVectorizer()
+
+# Generate the tf-idf vectors for the corpus
+tfidf_matrix = tfidf_vectorizer.fit_transform(corpus)
+
+# Compute and print the cosine similarity matrix
+cosine_sim = cosine_similarity(tfidf_matrix,tfidf_matrix)
+print(cosine_sim)
+
+## Comparing linear_kernel and cosine_similarity
+
+# Record start time
+start = time.time()
+
+# Compute cosine similarity matrix
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+# Print cosine similarity matrix
+print(cosine_sim)
+
+# Print time taken
+print("Time taken: %s seconds" %(time.time() - start))
+
+# Record start time
+start = time.time()
+
+# Compute cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+# Print cosine similarity matrix
+print(cosine_sim)
+
+# Print time taken
+print("Time taken: %s seconds" %(time.time() - start))
+
+## Plot recommendation engine
+
+# Initialize the TfidfVectorizer 
+tfidf = TfidfVectorizer(stop_words='english')
+
+# Construct the TF-IDF matrix
+tfidf_matrix = tfidf.fit_transform(movie_plots)
+
+# Generate the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+ 
+# Generate recommendations
+print(get_recommendations('The Dark Knight Rises', cosine_sim, indices))
+
+## The recommender function
+
+# Generate mapping between titles and index
+indices = pd.Series(metadata.index, index=metadata['title']).drop_duplicates()
+
+def get_recommendations(title, cosine_sim, indices):
+    # Get index of movie that matches title
+    idx = indices[title]
+    # Sort the movies based on the similarity scores
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    # Get the scores for 10 most similar movies
+    sim_scores = sim_scores[1:11]
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+    # Return the top 10 most similar movies
+    return metadata['title'].iloc[movie_indices]
+
+## TED talk recommender
+
+# Initialize the TfidfVectorizer 
+tfidf = TfidfVectorizer(stop_words='english')
+
+# Construct the TF-IDF matrix
+tfidf_matrix = tfidf.fit_transform(transcripts)
+
+# Generate the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+ 
+# Generate recommendations
+print(get_recommendations('5 ways to kill your dreams', cosine_sim, indices))
+
+## Generating word vectors
+
+# Create the doc object
+doc = nlp(sent)
+
+# Compute pairwise similarity scores
+for token1 in doc:
+  for token2 in doc:
+    print(token1.text, token2.text, token1.similarity(token2))
+
+## Computing similarity of Pink Floyd songs
+
+# Create Doc objects
+mother_doc = nlp(mother)
+hopes_doc = nlp(hopes)
+hey_doc = nlp(hey)
+
+# Print similarity between mother and hopes
+print(mother_doc.similarity(hopes_doc))
+
+# Print similarity between mother and hey
+print(mother_doc.similarity(hey_doc))
+
+## Congratulations!
