da47da9ae9bd7d6240a5c4a217fd68b25a6c83ec
sabirinID <108510388+sabirinID@users.noreply.github.com>
2023-11-07 09:39:52 +0700

    Upload file

---
+++ Tracks/Career Tracks/Machine Learning Scientist with Python/Machine Learning with PySpark/Machine Learning with PySpark.py
@@ -0,0 +1,594 @@
+## Characteristics of Spark
+
+# All of the above.
+
+## Components in a Spark Cluster
+
+# A load balancer
+# A load balancer distributes work across multiple resources, preventing overload on any one resource. In Spark this function is performed by the cluster manager.
+
+## Location of Spark master
+
+# spark://18.188.22.23
+# A Spark URL must always include a port number, so this URL is not valid.
+
+## Creating a SparkSession
+
+# Import the SparkSession class
+from pyspark.sql import SparkSession
+
+# Create SparkSession object
+spark = SparkSession.builder \
+                    .master('local[*]') \
+                    .appName('test') \
+                    .getOrCreate()
+
+# What version of Spark?
+print(spark.version)
+
+# Terminate the cluster
+spark.stop()
+
+## Loading flights data
+
+# Read data from CSV file
+flights = spark.read.csv('flights.csv',
+                         sep=',',
+                         header=True,
+                         inferSchema=True,
+                         nullValue='NA')
+
+# Get number of records
+print("The data contain %d records." % flights.count())
+
+# View the first five records
+flights.show(5)
+
+# Check column data types
+print(flights.dtypes)
+
+## Loading SMS spam data
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+# Specify column names and types
+schema = StructType([
+    StructField("id", IntegerType()),
+    StructField("text", StringType()),
+    StructField("label", IntegerType())
+])
+
+# Load data from a delimited file
+sms = spark.read.csv("sms.csv", sep=';', header=False, schema=schema)
+
+# Print schema of DataFrame
+sms.printSchema()
+
+## Removing columns and rows
+
+# Remove the 'flight' column
+flights_drop_column = flights.drop('flight')
+
+# Number of records with missing 'delay' values
+flights_drop_column.filter('delay IS NULL').count()
+
+# Remove records with missing 'delay' values
+flights_valid_delay = flights_drop_column.filter('delay IS NOT NULL')
+
+# Remove records with missing values in any column and get the number of remaining rows
+flights_none_missing = flights_valid_delay.dropna()
+print(flights_none_missing.count())
+
+## Column manipulation
+
+# Import the required function
+from pyspark.sql.functions import round
+
+# Convert 'mile' to 'km' and drop 'mile' column (1 mile is equivalent to 1.60934 km)
+flights_km = flights.withColumn('km', round(flights.mile * 1.60934, 0)) \
+                    .drop('mile')
+
+# Create 'label' column indicating whether flight delayed (1) or not (0)
+flights_km = flights_km.withColumn('label', (flights_km.delay >= 15).cast('integer'))
+
+# Check first five records
+flights_km.show(5)
+
+## Categorical columns
+
+from pyspark.ml.feature import StringIndexer
+
+# Create an indexer
+indexer = StringIndexer(inputCol='carrier', outputCol='carrier_idx')
+
+# Indexer identifies categories in the data
+indexer_model = indexer.fit(flights)
+
+# Indexer creates a new column with numeric index values
+flights_indexed = indexer_model.transform(flights)
+
+# Repeat the process for the other categorical feature
+flights_indexed = StringIndexer(inputCol='org', outputCol='org_idx').fit(flights_indexed).transform(flights_indexed)
+flights_indexed.show(5)
+
+## Assembling columns
+
+# Import the necessary class
+from pyspark.ml.feature import VectorAssembler
+
+# Create an assembler object
+assembler = VectorAssembler(inputCols=[
+    'mon', 'dom', 'dow', 'carrier_idx', 'org_idx', 'km', 'depart', 'duration'
+], outputCol='features')
+
+# Consolidate predictor columns
+flights_assembled = assembler.transform(flights)
+
+# Check the resulting column
+flights_assembled.select('features', 'delay').show(5, truncate=False)
+
+## Train/test split
+
+# Split into training and testing sets in a 80:20 ratio
+flights_train, flights_test = flights.randomSplit([0.8, 0.2], seed=43)
+
+# Check that training set has around 80% of records
+training_ratio = flights_train.count() / flights.count()
+print(training_ratio)
+
+## Build a Decision Tree
+
+# Import the Decision Tree Classifier class
+from pyspark.ml.classification import DecisionTreeClassifier
+
+# Create a classifier object and fit to the training data
+tree = DecisionTreeClassifier()
+tree_model = tree.fit(flights_train)
+
+# Create predictions for the testing data and take a look at the predictions
+prediction = tree_model.transform(flights_test)
+prediction.select('label', 'prediction', 'probability').show(5, False)
+
+## Evaluate the Decision Tree
+
+# Create a confusion matrix
+prediction.groupBy('label', 'prediction').count().show()
+
+# Calculate the elements of the confusion matrix
+TN = prediction.filter('prediction = 0 AND label = prediction').count()
+TP = prediction.filter('prediction = 1 AND label = prediction').count()
+FN = prediction.filter('prediction = 0 AND label != prediction').count()
+FP = prediction.filter('prediction = 1 AND label != prediction').count()
+
+# Accuracy measures the proportion of correct predictions
+accuracy = (TN + TP) / (TN + TP + FN + FP)
+print(accuracy)
+
+## Build a Logistic Regression model
+
+# Import the logistic regression class
+from pyspark.ml.classification import LogisticRegression
+
+# Create a classifier object and train on training data
+logistic = LogisticRegression().fit(flights_train)
+
+# Create predictions for the testing data and show confusion matrix
+prediction = logistic.transform(flights_test)
+prediction.groupBy('label', 'prediction').count().show()
+
+## Evaluate the Logistic Regression model
+
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator, BinaryClassificationEvaluator
+
+# Calculate precision and recall
+precision = TP / (TP + FP)
+recall = TP / (TP + FN)
+print('precision = {:.2f}\nrecall    = {:.2f}'.format(precision, recall))
+
+# Find weighted precision
+multi_evaluator = MulticlassClassificationEvaluator()
+weighted_precision = multi_evaluator.evaluate(prediction, {multi_evaluator.metricName: "weightedPrecision"})
+
+# Find AUC
+binary_evaluator = BinaryClassificationEvaluator()
+auc = binary_evaluator.evaluate(prediction, {binary_evaluator.metricName: "areaUnderROC"})
+
+## Punctuation, numbers and tokens
+
+# Import the necessary functions
+from pyspark.sql.functions import regexp_replace
+from pyspark.ml.feature import Tokenizer
+
+# Remove punctuation (REGEX provided) and numbers
+wrangled = sms.withColumn('text', regexp_replace(sms.text, '[_():;,.!?\\-]', ' '))
+wrangled = wrangled.withColumn('text', regexp_replace(wrangled.text, '[0-9]', ' '))
+
+# Merge multiple spaces
+wrangled = wrangled.withColumn('text', regexp_replace(wrangled.text, ' +', ' '))
+
+# Split the text into words
+wrangled = Tokenizer(inputCol='text', outputCol='words').transform(wrangled)
+
+wrangled.show(4, truncate=False)
+
+## Stop words and hashing
+
+from pyspark.ml.feature import StopWordsRemover, HashingTF, IDF
+
+# Remove stop words.
+wrangled = StopWordsRemover(inputCol='words', outputCol='terms')\
+      .transform(sms)
+
+# Apply the hashing trick
+wrangled = HashingTF(inputCol='terms', outputCol='hash', numFeatures=1024)\
+      .transform(wrangled)
+
+# Convert hashed symbols to TF-IDF
+tf_idf = IDF(inputCol='hash', outputCol='features')\
+      .fit(wrangled).transform(wrangled)
+      
+tf_idf.select('terms', 'features').show(4, truncate=False)
+
+## Training a spam classifier
+
+# Split the data into training and testing sets
+sms_train, sms_test = sms.randomSplit([0.8, 0.2], seed=13)
+
+# Fit a Logistic Regression model to the training data
+logistic = LogisticRegression(regParam=0.2).fit(sms_train)
+
+# Make predictions on the testing data
+prediction = logistic.transform(sms_test)
+
+# Create a confusion matrix, comparing predictions to known labels
+prediction.groupBy('label', 'prediction').count().show()
+
+## Encoding flight origin
+
+# Import the one hot encoder class
+from pyspark.ml.feature import OneHotEncoder
+
+# Create an instance of the one hot encoder
+onehot = OneHotEncoder(inputCols=['org_idx'], outputCols=['org_dummy'])
+
+# Apply the one hot encoder to the flights data
+onehot = onehot.fit(flights)
+flights_onehot = onehot.transform(flights)
+
+# Check the results
+flights_onehot.select('org', 'org_idx', 'org_dummy').distinct().sort('org_idx').show()
+
+## Encoding shirt sizes
+
+# XL shirts get index 3.0 and are one-hot encoded as (3,[3],[1.0])
+# Correct! This statement is false: XL is the least frequent size, so it receives an index of 3. However, it is one-hot encoded to (3,[],[]) because it does not get it's own dummy variable. If none of the other dummy variables are true, then this one must be true. So to make a separate dummy variable would be redundant!
+
+## Flight duration model: Just distance
+
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+
+# Create a regression object and train on training data
+regression = LinearRegression(labelCol='duration').fit(flights_train)
+
+# Create predictions for the testing data and take a look at the predictions
+predictions = regression.transform(flights_test)
+predictions.select('duration', 'prediction').show(5, False)
+
+# Calculate the RMSE
+RegressionEvaluator(labelCol='duration').evaluate(predictions)
+
+## Interpreting the coefficients
+
+# Intercept (average minutes on ground)
+inter = regression.intercept
+print(inter)
+
+# Coefficients
+coefs = regression.coefficients
+print(coefs)
+
+# Average minutes per km
+minutes_per_km = regression.coefficients[0]
+print(minutes_per_km)
+
+# Average speed in km per hour
+avg_speed = 60 / minutes_per_km
+print(avg_speed)
+
+## Flight duration model: Adding origin airport
+
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+
+# Create a regression object and train on training data
+regression = LinearRegression(labelCol='duration').fit(flights_train)
+
+# Create predictions for the testing data
+predictions = regression.transform(flights_test)
+
+# Calculate the RMSE on testing data
+RegressionEvaluator(labelCol='duration').evaluate(predictions)
+
+## Interpreting coefficients
+
+# Average speed in km per hour
+avg_speed_hour = 60 / regression.coefficients[0]
+print(avg_speed_hour)
+
+# Average minutes on ground at OGG
+inter = regression.intercept
+print(inter)
+
+# Average minutes on ground at JFK
+avg_ground_jfk = inter + regression.coefficients[3]
+print(avg_ground_jfk)
+
+# Average minutes on ground at LGA
+avg_ground_lga = inter + regression.coefficients[4]
+print(avg_ground_lga)
+
+## Bucketing departure time
+
+from pyspark.ml.feature import Bucketizer, OneHotEncoder
+
+# Create buckets at 3 hour intervals through the day
+buckets = Bucketizer(splits=[0, 3, 6, 9, 12, 15, 18, 21, 24], inputCol='depart', outputCol='depart_bucket')
+
+# Bucket the departure times
+bucketed = buckets.transform(flights)
+bucketed.select('depart', 'depart_bucket').show(5)
+
+# Create a one-hot encoder
+onehot = OneHotEncoder(inputCols=['depart_bucket'], outputCols=['depart_dummy'])
+
+# One-hot encode the bucketed departure times
+flights_onehot = onehot.fit(bucketed).transform(bucketed)
+flights_onehot.select('depart', 'depart_bucket', 'depart_dummy').show(5)
+
+## Flight duration model: Adding departure time
+
+# Find the RMSE on testing data
+from pyspark.ml.evaluation import RegressionEvaluator
+rmse = RegressionEvaluator(labelCol='duration').evaluate(predictions)
+print("The test RMSE is", rmse)
+
+# Average minutes on ground at OGG for flights departing between 21:00 and 24:00
+avg_eve_ogg = regression.intercept
+print(avg_eve_ogg)
+
+# Average minutes on ground at OGG for flights departing between 03:00 and 06:00
+avg_night_ogg = regression.intercept + regression.coefficients[9]
+print(avg_night_ogg)
+
+# Average minutes on ground at JFK for flights departing between 03:00 and 06:00
+avg_night_jfk = regression.intercept + regression.coefficients[9] + regression.coefficients[3]
+print(avg_night_jfk)
+
+## Flight duration model: More features!
+
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+
+# Fit linear regression model to training data
+regression = LinearRegression(labelCol='duration').fit(flights_train)
+
+# Make predictions on testing data
+predictions = regression.transform(flights_test)
+
+# Calculate the RMSE on testing data
+rmse = RegressionEvaluator(labelCol='duration').evaluate(predictions)
+print("The test RMSE is", rmse)
+
+# Look at the model coefficients
+coeffs = regression.coefficients
+print(coeffs)
+
+## Flight duration model: Regularization!
+
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+
+# Fit Lasso model (λ = 1, α = 1) to training data
+regression = LinearRegression(labelCol='duration', regParam=1, elasticNetParam=1).fit(flights_train)
+
+# Calculate the RMSE on testing data
+rmse = RegressionEvaluator(labelCol='duration').evaluate(regression.transform(flights_test))
+print("The test RMSE is", rmse)
+
+# Look at the model coefficients
+coeffs = regression.coefficients
+print(coeffs)
+
+# Number of zero coefficients
+zero_coeff = sum([beta == 0 for beta in regression.coefficients])
+print("Number of coefficients equal to 0:", zero_coeff)
+
+## Flight duration model: Pipeline stages
+
+# Convert categorical strings to index values
+indexer = StringIndexer(inputCol='org', outputCol='org_idx')
+
+# One-hot encode index values
+onehot = OneHotEncoder(
+    inputCols=['org_idx', 'dow'],
+    outputCols=['org_dummy', 'dow_dummy']
+)
+
+# Assemble predictors into a single column
+assembler = VectorAssembler(inputCols=['km', 'org_dummy', 'dow_dummy'], outputCol='features')
+
+# A linear regression object
+regression = LinearRegression(labelCol='duration')
+
+## Flight duration model: Pipeline model
+
+# Import class for creating a pipeline
+from pyspark.ml import Pipeline
+
+# Construct a pipeline
+pipeline = Pipeline(stages=[indexer, onehot, assembler, regression])
+
+# Train the pipeline on the training data
+pipeline = pipeline.fit(flights_train)
+
+# Make predictions on the testing data
+predictions = pipeline.transform(flights_test)
+
+## SMS spam pipeline
+
+from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF
+
+# Break text into tokens at non-word characters
+tokenizer = Tokenizer(inputCol='text', outputCol='words')
+
+# Remove stop words
+remover = StopWordsRemover(inputCol=tokenizer.getOutputCol(), outputCol='terms')
+
+# Apply the hashing trick and transform to TF-IDF
+hasher = HashingTF(inputCol=remover.getOutputCol(), outputCol="hash")
+idf = IDF(inputCol=hasher.getOutputCol(), outputCol="features")
+
+# Create a logistic regression object and add everything to a pipeline
+logistic = LogisticRegression()
+pipeline = Pipeline(stages=[tokenizer, remover, hasher, idf, logistic])
+
+## Cross validating simple flight duration model
+
+# Create an empty parameter grid
+params = ParamGridBuilder().build()
+
+# Create objects for building and evaluating a regression model
+regression = LinearRegression(labelCol='duration')
+evaluator = RegressionEvaluator(labelCol='duration')
+
+# Create a cross validator
+cv = CrossValidator(estimator=regression, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)
+
+# Train and test model on multiple folds of the training data
+cv = cv.fit(flights_train)
+
+# NOTE: Since cross-validation builds multiple models, the fit() method can take a little while to complete.
+
+## Cross validating flight duration model pipeline
+
+# Create an indexer for the org field
+indexer = StringIndexer(inputCol='org', outputCol='org_idx')
+
+# Create an one-hot encoder for the indexed org field
+onehot = OneHotEncoder(inputCols=['org_idx'], outputCols=['org_dummy'])
+
+# Assemble the km and one-hot encoded fields
+assembler = VectorAssembler(inputCols=['km', 'org_dummy'], outputCol='features')
+
+# Create a pipeline and cross-validator.
+pipeline = Pipeline(stages=[indexer, onehot, assembler, regression])
+cv = CrossValidator(estimator=pipeline,
+                    estimatorParamMaps=params,
+                    evaluator=evaluator)
+
+## Optimizing flights linear regression
+
+# Create parameter grid
+params = ParamGridBuilder()
+
+# Add grids for two parameters
+params = params.addGrid(regression.regParam, [0.01, 0.1, 1.0, 10.0]) \
+               .addGrid(regression.elasticNetParam, [0.0, 0.5, 1.0])
+
+# Build the parameter grid
+params = params.build()
+print('Number of models to be tested: ', len(params))
+
+# Create cross-validator
+cv = CrossValidator(estimator=pipeline, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)
+
+## Dissecting the best flight duration model
+
+# Get the best model from cross validation
+best_model = cv.bestModel
+
+# Look at the stages in the best model
+print(best_model.stages)
+
+# Get the parameters for the LinearRegression object in the best model
+best_model.stages[3].extractParamMap()
+
+# Generate predictions on testing data using the best model then calculate RMSE
+predictions = best_model.transform(flights_test)
+print("RMSE =", evaluator.evaluate(predictions))
+
+## SMS spam optimised
+
+# Create parameter grid
+params = ParamGridBuilder()
+
+# Add grid for hashing trick parameters
+params = params.addGrid(hasher.numFeatures, [1024, 4096, 16384]) \
+               .addGrid(hasher.binary, [True, False])
+
+# Add grid for logistic regression parameters
+params = params.addGrid(logistic.regParam, [0.01, 0.1, 1.0, 10.0]) \
+               .addGrid(logistic.elasticNetParam, [0.0, 0.5, 1.0])
+
+# Build parameter grid
+params = params.build()
+
+## How many models for grid search?
+
+# 360
+# Correct! There are 72 points in the parameter grid and 5 folds in the cross-validator. The product is 360. It takes time to build all of those models, which is why you're not doing it here!
+
+## Delayed flights with Gradient-Boosted Trees
+
+# Import the classes required
+from pyspark.ml.classification import DecisionTreeClassifier, GBTClassifier
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+# Create model objects and train on training data
+tree = DecisionTreeClassifier().fit(flights_train)
+gbt = GBTClassifier().fit(flights_train)
+
+# Compare AUC on testing data
+evaluator = BinaryClassificationEvaluator()
+print(evaluator.evaluate(tree.transform(flights_test)))
+print(evaluator.evaluate(gbt.transform(flights_test)))
+
+# Find the number of trees and the relative importance of features
+print(gbt.getNumTrees)
+print(gbt.featureImportances)
+
+## Delayed flights with a Random Forest
+
+# Create a random forest classifier
+forest = RandomForestClassifier()
+
+# Create a parameter grid
+params = ParamGridBuilder() \
+            .addGrid(forest.featureSubsetStrategy, ['all', 'onethird', 'sqrt', 'log2']) \
+            .addGrid(forest.maxDepth, [2, 5, 10]) \
+            .build()
+
+# Create a binary classification evaluator
+evaluator = BinaryClassificationEvaluator()
+
+# Create a cross-validator
+cv = CrossValidator(estimator=forest, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)
+
+## Evaluating Random Forest
+
+# Average AUC for each parameter combination in grid
+print(cv.avgMetrics)
+
+# Average AUC for the best model
+print(max(cv.avgMetrics))
+
+# What's the optimal parameter value for maxDepth?
+print(cv.bestModel.explainParam('maxDepth'))
+# What's the optimal parameter value for featureSubsetStrategy?
+print(cv.bestModel.explainParam('featureSubsetStrategy'))
+
+# AUC for best model on testing data
+print(evaluator.evaluate(cv.transform(flights_test)))
+
+## Closing thoughts
